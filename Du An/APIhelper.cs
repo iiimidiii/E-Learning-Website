ba0134990e3e3f08335e5b29539a8bd7349afd58
iiimidiii <82705356+iiimidiii@users.noreply.github.com>
2022-06-13 09:40:09 +0700

    6/13

---
+++ Du An/APIhelper.cs
@@ -0,0 +1,14 @@
+﻿namespace Du_An
+{
+    public class APIhelper
+    {
+      public static HttpClient APIClient { get; set; } = new HttpClient();
+        public static void InitializeClient()
+        {
+            APIClient = new HttpClient();
+            APIClient.DefaultRequestHeaders.Accept.Clear();
+            APIClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+        }
+    }
+}
